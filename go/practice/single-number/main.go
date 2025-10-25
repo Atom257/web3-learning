001273ad72ae1cf1e9614923ebc268f9050e7b5a
Atom257 <jiax520@gmail.com>
2025-10-25 20:55:26 +0800

    Update main.go

--- go/practice/single-number/main.go
+++ go/practice/single-number/main.go
@@ -1,1 +1,17 @@
+package main
+
+import "fmt"
+
+func singleNumber(nums []int) int {
+    res := 0
+    for _, v := range nums {
+        res ^= v
+    }
+    return res
+}
+
+func main() {
+    nums := []int{4, 1, 2, 1, 2}
+    fmt.Println(singleNumber(nums)) // Output: 4
+}
 
